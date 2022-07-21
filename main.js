98cf0d6a81701085b9c8b335c60c3f41f2cb4778
Josphat Otieno <josphathumphrey5@gmail.com>
2022-07-21 15:53:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const choices = ['Rock','Paper','Scissors'];
-let playerScore = 0;
-let computerScore = 0;
+let playerScore;
+let computerScore;
 
 function getComputerChoice() {
     // console.log(choices[Math.floor(Math.random() * 3)]);
@@ -11,30 +11,47 @@ function getComputerChoice() {
 
 const computerSelection = getComputerChoice().toLowerCase();
 const playerSelection = 'RoCk'.toLowerCase();
-// const playerSelection = prompt("What's your choice?");
-// console.log(playerSelection)
+// const playerSelection = prompt("What's your choice?").toLowerCase();
+console.log(playerSelection)
 
-// console.log(computerSelection);
+console.log(computerSelection);
 
 function playRound(playerSelection,computerSelection) {
     if(playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore += 1;
-        console.log('You win');
+        // console.log(playerScore,computerScore);
+        // console.log('You win');
         return 'You Win! Rock beats scissors';
         
     } else if(playerSelection === 'rock' && computerSelection === 'paper') {
         computerScore += 1;
-        console.log('You lose');
+        // console.log(playerScore,computerScore);
+        // console.log('You lose');
         return 'You Lose! Paper beats Rock';
         
+    } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Scissors beats Paper'
+
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper beats Rock'
+
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors beats Paper'
+
+    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock beats Scissors'
+
     } else if(playerSelection === computerSelection) {
         // console.log(computerSelection);
         playerScore;
         computerScore;
-        console.log('You tie');
+        // console.log(playerScore,computerScore);
+        // console.log('You tie');
         return 'You tie! Play again';
         
     }
+
+    // console.log(playerScore,computerScore);
 
     // return {
     //     playerScore: playerScore,
@@ -43,31 +60,17 @@ function playRound(playerSelection,computerSelection) {
     
 }
 
-// playRound(playerSelection,computerSelection);
+
+
 
 function game() {
-    // let playerScore = 0;
-    // let computerScore = 0;
 
-    // for (let i = 0; i < 2; i++) {
-    //     playRound(playerSelection,computerSelection);
-    //  }
-
-    //  const rounds = [1,2,3,4,5];
-
-    //  rounds.forEach(round => {
-    //     console.log(playRound(playerSelection,computerSelection)); 
-    
-    //  })
-
-    playRound(playerSelection,computerSelection);
+    console.log(playRound(playerSelection,computerSelection));
+    for (let i = 0; i < 5; i++) {
+         
+     }
 
 }
 
-let rounds = 5;
-
-while(rounds > 0) {
-    game();
-    rounds--;
-}
+game();
 
